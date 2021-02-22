2c2f9da59df4f4048cb83475eb301638d1f1e3ac
palfigergo <palfi.balaton@gmail.com>
2021-02-22 21:31:18 +0100

    double.MIN_VALUE

---
+++ src/main/java/GFA/Interview/Double/Main.java
@@ -0,0 +1,11 @@
+package GFA.Interview.Double;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+      //
+      System.out.println(Math.min(Double.MIN_VALUE, 0.0d));
+      System.out.println(Double.MIN_VALUE);
+    }
+}
